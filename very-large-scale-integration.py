bc8605232fb4240e45a40b043283e42d086b1179
Martino Pulici <martinomarelakota@yahoo.it>
2021-08-23 11:54:59 +0200

    Change cp model
     * out/*: delete output
     * src/cp.mzn: rename from src/m1.mzn
     * src/smt.py: minor changes
     * very-large-scale-integration.py: adapt to new cp model

--- very-large-scale-integration.py
+++ very-large-scale-integration.py
@@ -6,12 +6,23 @@ import numpy as np
 from matplotlib.patches import Rectangle
 from src.smt import *
 
+MIN = 1
+MAX = 20
 
+CP = True
+SMT = False
 
 def plot(file, width, height, blocks):
     _, ax = plt.subplots()
+    index = 1
     for w,h,x,y,c in blocks:
-        ax.add_patch(Rectangle((x,y), w, h, facecolor=c, lw=1))
+        ax.add_patch(Rectangle((x,y), w, h, facecolor=c))
+        ax.add_patch(Rectangle((x,y), w,h, fill=None, ls='-',lw=2))
+        # for j in range(w):
+        #     for k in range(h):
+        #         ax.text(x+j+1/2,y+k+1/2,str(index),horizontalalignment='center',verticalalignment='center')        
+        index += 1
+
 
     ax.set_ylim(0, height)
     ax.set_xlim(0, width)
@@ -20,12 +31,15 @@ def plot(file, width, height, blocks):
     ax.set_xticks(range(width+1))
     ax.set_yticks(range(height+1))
     ax.grid(color = 'black')
+    plt.title(str(file))
     plt.savefig('out/out-'+file+'.png')
+    plt.close()
 
 times_cp = []
 times_smt = []
-MAX = 5
-for i in range(1,MAX+1):
+model = Model("src/cp.mzn")
+solver = Solver.lookup("chuffed")
+for i in range(MIN,MAX+1):
     file = str(i)
     with open('ins/ins-'+file+'.txt', 'r') as infile:
         chip_w = int(infile.readline().strip())
@@ -39,50 +53,72 @@ for i in range(1,MAX+1):
             inst_y.append(int(line_split[1]))
             line = infile.readline()
 
-    model = Model("src/m1.mzn")
-    solver = Solver.lookup("gecode")
-    instance = Instance(solver, model)
-    instance['chip_w'] = chip_w
-    instance['n'] = n
-    instance['inst_x'] = inst_x
-    instance['inst_y'] = inst_y
+    if CP == True:
+        instance = Instance(solver, model)
+        instance['chip_w'] = chip_w
+        instance['n'] = n
+        instance['inst_x'] = inst_x
+        instance['inst_y'] = inst_y
 
-    result = instance.solve(timeout=datetime.timedelta(seconds=300))
+        result = instance.solve(timeout=datetime.timedelta(seconds=60),optimisation_level=5,free_search=True)
 
-    chip_h_cp = result['objective']
-    bl_x_cp = result['bl_x']
-    bl_y_cp = result['bl_y']
+        try:
+            chip_h_cp = result['objective']
+            bl_x_cp = result['bl_x']
+            bl_y_cp = result['bl_y']
+            if chip_h_cp == chip_w:
+                time_cp = result.statistics['time'].total_seconds()
+                times_cp.append(time_cp)
 
-    time_cp = result.statistics['time'].total_seconds()
-    times_cp.append(time_cp)
+                output_cp = str(chip_w) + " " + str(chip_h_cp) + "\n" + str(n) + "\n"
+                for j in range(n):
+                    output_cp += str(inst_x[j]) + " " + str(inst_y[j]) + " " + str(bl_x_cp[j]) + " " + str(bl_y_cp[j]) + "\n"
 
-    output_cp = str(chip_w) + " " + str(chip_h_cp) + "\n" + str(n) + "\n"
-    for j in range(n):
-        output_cp += str(inst_x[j]) + " " + str(inst_y[j]) + " " + str(bl_x_cp[j]) + " " + str(bl_y_cp[j]) + "\n"
+                with open('out/out-'+file+'-cp.txt', 'w') as outfile:
+                    outfile.write(output_cp)
 
-    with open('out/out-'+file+'-cp.txt', 'w') as outfile:
-        outfile.write(output_cp)
+                plot(file+'-cp', chip_w, chip_h_cp, [(inst_x[j], inst_y[j], bl_x_cp[j], bl_y_cp[j], tuple(value/255 for value in colorsys.hsv_to_rgb(j/n,0.75,191))) for j in range(n)])
+                print("DONE CP "+str(i)+": "+str(time_cp)+" s")
+            else:
+                time_cp = 0
+                times_cp.append(time_cp)
+                print("FAIL CP "+str(i))
+        except:
+            time_cp = 0
+            times_cp.append(time_cp)
+            print("FAIL CP "+str(i))
 
-    plot(file+'-cp', chip_w, chip_h_cp, [(inst_x[j], inst_y[j], bl_x_cp[j], bl_y_cp[j], tuple(value/255 for value in colorsys.hsv_to_rgb(j/n,0.75,191))) for j in range(n)])
+    if SMT == True:
+        chip_h_smt, bl_x_sat, bl_y_sat, time_smt = smt(chip_w, n, inst_x, inst_y)
+        if chip_h_smt == chip_w:
+            times_smt.append(time_smt)
+            output_sat = str(chip_w) + " " + str(chip_h_smt) + "\n" + str(n) + "\n"
+            for j in range(n):
+                output_sat += str(inst_x[j]) + " " + str(inst_y[j]) + " " + str(bl_x_sat[j]) + " " + str(bl_y_sat[j]) + "\n"
 
-    chip_h_sat, bl_x_sat, bl_y_sat, time_smt = smt(chip_w, n, inst_x, inst_y)
-    times_smt.append(time_smt)
-    output_sat = str(chip_w) + " " + str(chip_h_sat) + "\n" + str(n) + "\n"
-    for j in range(n):
-        output_sat += str(inst_x[j]) + " " + str(inst_y[j]) + " " + str(bl_x_sat[j]) + " " + str(bl_y_sat[j]) + "\n"
+            with open('out/out-'+file+'-smt.txt', 'w') as outfile:
+                outfile.write(output_sat)
 
-    with open('out/out-'+file+'-smt.txt', 'w') as outfile:
-        outfile.write(output_sat)
+            plot(file+'-smt', chip_w, chip_h_smt, [(inst_x[j], inst_y[j], bl_x_sat[j], bl_y_sat[j], tuple(value/255 for value in colorsys.hsv_to_rgb(j/n,0.75,191))) for j in range(n)])
+            print("DONE SMT "+str(i)+": "+str(time_smt)+" s")
+        else:
+            time_smt = 0
+            times_smt.append(time_smt)
+            print("FAIL SMT "+str(i))
 
-    plot(file+'-smt', chip_w, chip_h_sat, [(inst_x[j], inst_y[j], bl_x_sat[j], bl_y_sat[j], tuple(value/255 for value in colorsys.hsv_to_rgb(j/n,0.75,191))) for j in range(n)])
 
-
-_, ax = plt.subplots()
-ax.bar(np.arange(1,MAX+1)-0.2,times_cp,0.4,label='cp')
-ax.bar(np.arange(1,MAX+1)+0.2,times_smt,0.4,label='smt')
-ax.set_xticks(range(1,MAX+1))
-ax.grid(True)
+_, ax = plt.subplots(1,1,figsize=(10,10))
+if CP == True:
+    ax.bar(np.arange(MIN,MAX+1)-0.2,times_cp,0.4,label='cp')
+if SMT == True:
+    ax.bar(np.arange(MIN,MAX+1)+0.2,times_smt,0.4,label='smt')
+ax.set_xticks(range(MIN,MAX+1))
 ax.set_xlabel('instance')
 ax.set_ylabel('seconds')
+plt.yscale("log")
+plt.tick_params(axis='y', which='both')
+plt.grid(axis="y",which="both")
 plt.legend(loc='best')
+plt.title("Times")
 plt.savefig('out/times.png')
+plt.close()
