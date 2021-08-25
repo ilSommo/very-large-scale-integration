9788196c23d0cabec22e877a5a7ebfaf3c1b9d6a
Martino Pulici <martinomarelakota@yahoo.it>
2021-08-25 22:09:14 +0200

    Add documentation
     * CODE_OF_CONDUCT.md: add file
     * LICENSE: minor fix
     * README.md: add documentation instructions
     * makefile: add documentation
     * out/*: output files
     * src/*: add documentation
     * very-large-scale-integration.py: add documentation
     * vlsi/*: add documentation

--- vlsi/solvers/smt.py
+++ vlsi/solvers/smt.py
@@ -6,8 +6,35 @@ import time
 
 import z3
 
-def smt(data, timeout, rotation):
 
+def smt(data, timeout, rotation):
+    """Solves a SMT problem.
+
+    Parameters
+    ----------
+    data : dict
+        Data dictionary.
+    timeout : int
+        Timeout in seconds.
+    rotation : bool
+        Flag for rotation.
+
+    Returns
+    -------
+    chip_h_int : int
+        Chip height.
+    result_x : int
+        Blocks' horizontal positions.
+    result_y : int
+        Blocks' vertical positions.
+    result_inst_x : int
+        Blocks' widths.
+    result_inst_y : int
+        Blocks' heights.
+    computation_time : float
+        Computation time in seconds.
+    """
+    # Instance parameters
     chip_w = data['chip_w']
     n = data['n']
     inst_x =  data['inst_x']
@@ -16,54 +43,79 @@ def smt(data, timeout, rotation):
     max_h = data['max_h']
     min_index = data['min_index']
 
+    # z3 optimizer
     opt = z3.Optimize()
     opt.set('timeout', timeout*1000)
+    # Chip height
     chip_h = z3.Int("chip_h")
 
+    # Blocks' positions
     bl_x = z3.IntVector("bl_x", n)
     bl_y = z3.IntVector("bl_y", n)
     
+    # Enter if rotation is enabled
     if rotation == True:
+        # Boolean vector to flag rotated blocks
         rotated = z3.BoolVector("rotated", n)
+        # Actual blocks' widths and heights
         new_inst_x = [z3.If(rotated[k], inst_y[k], inst_x[k]) for k in range(n)]
         new_inst_y = [z3.If(rotated[k], inst_x[k], inst_y[k]) for k in range(n)]
     else:
+        # Actual blocks' widths and heights
         new_inst_x = inst_x
         new_inst_y = inst_y
 
-
+    # Cycle blocks
     for k in range(n):
+        # Add constraints on blocks' positions
         opt.add(bl_x[k] >=0)
-        opt.add(bl_x[k]+new_inst_x[k] <= chip_w)
+        opt.add(bl_x[k] <= chip_w-new_inst_x[k])
         opt.add(bl_y[k] >=0)
-        opt.add(bl_y[k] + new_inst_y[k]<=chip_h)
+        opt.add(bl_y[k] <=chip_h-new_inst_y[k])
+        # Cycle blocks
         for l in range(n):
+            # Enter if indexes are different
             if k != l:
+                # Add reciprocal positional constraints
                 opt.add(z3.Or((bl_x[l]+new_inst_x[l] <= bl_x[k]),(bl_x[l] >= bl_x[k]+new_inst_x[k]),(bl_y[l]+new_inst_y[l] <= bl_y[k]),(bl_y[l] >= bl_y[k]+new_inst_y[k])))
 
+    # Add chip height constraints
     opt.add(chip_h <= max_h)
     opt.add(chip_h >= min_h)
     
-    # symmetry breaking
+    # Add symmetry breaking constraints
     opt.add(z3.And(((2*bl_x[min_index]+new_inst_x[min_index])<=chip_w),((2*bl_y[min_index]+new_inst_y[min_index])<=chip_h)))
 
+    # Minimize chip height
     opt.minimize(chip_h)
+    # Start timer
     start = time.time() 
+    # Check SMT
     check = opt.check()
+    # Stop timer
     end = time.time()
+    # Save model
     model=opt.model()
+    # Result lists
     result_x=[]
     result_y=[]
     result_inst_x=[]
     result_inst_y=[]
+    # Enter if optimal solution is found
     if check == z3.sat:
+        # Cycle chips
         for k in range(n):
+            # Append results
             result_x.append(int(str(model.evaluate(bl_x[k]))))
             result_y.append(int(str(model.evaluate(bl_y[k]))))
             result_inst_x.append(int(str(model.evaluate(new_inst_x[k]))) if rotation == True else new_inst_x[k])
             result_inst_y.append(int(str(model.evaluate(new_inst_y[k]))) if rotation == True else new_inst_y[k])
+        # Chip height
         chip_h_int = int(str(model.evaluate(chip_h)))
     else:
+        # Chip null height
         chip_h_int = None
 
-    return chip_h_int, result_x, result_y, result_inst_x, result_inst_y, end-start
+    # Computation time
+    computation_time = end-start
+    return chip_h_int, result_x, result_y,result_inst_x, result_inst_y, computation_time
--- vlsi/utilities/preprocessing.py
+++ vlsi/utilities/preprocessing.py
@@ -4,43 +4,97 @@ __author__ = 'Giacomo Berselli, Martino Pulici'
 
 import numpy as np
 
+
+def compute_max_h(chip_w, inst_x, inst_y):
+    """Calculates maximum chip height.
+
+    Parameters
+    ----------
+    chip_w : int
+        Chip width.
+    inst_x : list
+        Blocks' widths.
+    inst_y : list
+        Blocks' heights.
+
+    Returns
+    -------
+    max_h : int
+        Maximum chip height.
+    """
+    # Maximum height
+    max_h = 0
+    # Blocks' widths and heights
+    inst = zip(inst_x, inst_y)
+    # Cycle while there are blocks left
+    while inst:
+        # Sort blocks according to height and width
+        inst = sorted(inst, key=lambda tup: tup[0], reverse=True)
+        inst = sorted(inst, key=lambda tup: tup[1], reverse=True)
+        # Width left
+        chip_cumulative = chip_w
+        # Block index
+        k = 0
+        # Heights list
+        heights = []
+        # Cycle while there are blocks
+        while k < len(inst):
+            # Enter if there is space for the block
+            if inst[k][0] <= chip_cumulative:
+                # Subtract width
+                chip_cumulative -= inst[k][0]
+                # Append block height
+                heights.append(inst[k][1])
+                # Remove used block
+                del inst[k]
+            else:
+                # Increase index
+                k += 1
+        # Increase maximum height
+        max_h += max(heights)
+    return max_h
+
+
+
 def preprocessing(file):
+    """Preprocesses the file.
+
+    Parameters
+    ----------
+    file : string
+        File name.
+
+    Returns
+    -------
+    data : dict
+        Processed data.
+    """
+    # Open input file
     with open("ins/ins-" + file + ".txt", 'r') as infile:
+        # Width of chip
         chip_w = int(infile.readline().strip())
+        # Number of blocks
         n = int(infile.readline().strip())
+        # Blocks' widths and heigths
         inst_x = []
         inst_y = []
         line = infile.readline()
+        # Read blocks' widths and heigths
         while line.strip():
             line_split = (line.strip().split(' '))
             inst_x.append(int(line_split[0]))
             inst_y.append(int(line_split[1]))
             line = infile.readline()
-
+    # Index of smallest block
     min_index = int(np.argmin([inst_x[k]*inst_y[k] for k in range(n)]))
 
-    total_area = sum([inst_x[k]*inst_y[k] for k in range(n)])
-
-    min_h = total_area // chip_w
+    # Minimum height of chip
+    min_h = sum([inst_x[k]*inst_y[k] for k in range(n)]) // chip_w
     
-    max_h = 0
-    inst = zip(inst_x, inst_y)
-    while inst:
-        inst = sorted(inst, key=lambda tup: tup[0], reverse=True)
-        inst = sorted(inst, key=lambda tup: tup[1], reverse=True)
-        chip_cumulative = chip_w
-        tiles = 0
-        k = 0
-        heights = []
-        while k < len(inst):
-            if inst[k][0] <= chip_cumulative:
-                chip_cumulative -= inst[k][0]
-                tiles += 1
-                heights.append(inst[k][1])
-                del inst[k]
-            else:
-                k += 1
-        max_h += max(heights)
+    # Maximum height of chip
+    max_h = compute_max_h(chip_w, inst_x, inst_y)
+    
+    # Data dictionary
     data={}
     data['chip_w'] = chip_w
     data['n'] = n
